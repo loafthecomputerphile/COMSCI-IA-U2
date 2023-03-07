089c9f8ec48fd7f754acd202c114512403f2d2db
Loaf the Computerphile <53319491+drewquashie@users.noreply.github.com>
2023-03-07 09:49:27 -0400

    Add files via upload

--- CS_U2_IA/main.c
+++ CS_U2_IA/main.c
@@ -4,107 +4,9 @@
 #include <stdbool.h>
 #include <time.h>
 
-
-
+#define USERFILE "UserDataBase.bin"
+#define IDFILE "IdDataBase.bin"
 //=================================================HASH MAP==========================================================
-#define SIZE 100000
-#define MAX_KEY_LENGTH 110
-
-struct DataItem {
-    int value;
-    char key[MAX_KEY_LENGTH];
-};
-
-struct DataItem* hashArray[SIZE];
-struct DataItem* dummyItem;
-struct DataItem* item;
-
-int hashCode(char* key) {
-    int sum = 0;
-    int i;
-    for (i = 0; i < strlen(key); i++) {
-        sum += key[i];
-    }
-    return sum % SIZE;
-}
-
-struct DataItem* search(char* key) {
-    int hashIndex = hashCode(key);
-    while (hashArray[hashIndex] != NULL) {
-        if (strcmp(hashArray[hashIndex]->key, key) == 0) {
-            return hashArray[hashIndex];
-        }
-        hashIndex = (hashIndex + 1) % SIZE;
-    }
-    return NULL;
-}
-
-void insert(char* key, int value) {
-    struct DataItem* item = (struct DataItem*)malloc(sizeof(struct DataItem));
-    strcpy(item->key, key);
-    item->value = value;
-    int hashIndex = hashCode(key);
-    while (hashArray[hashIndex] != NULL && strcmp(hashArray[hashIndex]->key, key) != 0) {
-        hashIndex = (hashIndex + 1) % SIZE;
-    }
-    hashArray[hashIndex] = item;
-}
-
-struct DataItem* delete(struct DataItem* item) {
-    char* key = item->key;
-    int hashIndex = hashCode(key);
-    while (hashArray[hashIndex] != NULL) {
-        if (strcmp(hashArray[hashIndex]->key, key) == 0) {
-            struct DataItem* temp = hashArray[hashIndex];
-            hashArray[hashIndex] = dummyItem;
-            return temp;
-        }
-        hashIndex = (hashIndex + 1) % SIZE;
-    }
-    return NULL;
-}
-
-void display() {
-    int i;
-    for (i = 0; i < SIZE; i++) {
-        if (hashArray[i] != NULL) {
-            printf(" (%s,%d)", hashArray[i]->key, hashArray[i]->value);
-        }
-    }
-    printf("\n");
-}
-
-void writeToFile(char* filename) {
-    FILE* fp = fopen(filename, "wb");
-    if (fp == NULL) {
-        printf("Error opening file!\n");
-        return;
-    }
-    int i;
-    for (i = 0; i < SIZE; i++) {
-        if (hashArray[i] != NULL) {
-            fwrite(hashArray[i], sizeof(struct DataItem), 1, fp);
-        }
-    }
-    fclose(fp);
-}
-
-void readFromFile(char* filename) {
-    FILE* fp = fopen(filename, "rb");
-    if (fp == NULL) {
-        printf("Error opening file!\n");
-        return;
-    }
-    int i;
-    struct DataItem temp;
-    while (fread(&temp, sizeof(struct DataItem), 1, fp) == 1) {
-        insert(temp.key, temp.value);
-    }
-    fclose(fp);
-}
-
-
-//===================================================================================================================
 
 typedef struct{
 	int itemid;
@@ -114,18 +16,6 @@ typedef struct{
     float cost_price;
 } Item;
 
-Item CreateNewItem(int id, char name[], int stock, float selling, float cost){
-	
-	Item item;
-	
-	item.itemid = id;
-	item.instock = stock;
-	item.selling_price = selling;
-	item.cost_price = cost; 
-	strcpy(item.name, name);
-	
-	return item;
-}
 
 //define the sale data structure
 typedef struct{
@@ -134,43 +24,23 @@ typedef struct{
 	time_t unix_time;
 } Sale;
 
-Sale CreateNewSale(int id, User user){
-	Sale sale;
-	sale.saleid = id;
-	sale.userid = user.userid;
-	sale.unix_time = time(NULL);
-	return sale;
-}
+
 
  
 typedef struct{
 	int itemid;
 	int quantity;
     float cost;
-    int success = 1;
+    int success;
 } SoldItem;
 
-SoldItem CreateNewSoldItem(Item item, int quantity){
-	
-	SoldItem solditem;
-	
-	
-	if (item.instock < 1 || item.instock == NULL){
-		solditem.success = 0;
-		return solditem;
-	}else if (quantity < 1){
-		solditem.success = 0;
-		return solditem;
-	}
-	
-	solditem.itemid = item.id;
-	solditem.quantity = quantity;
-	solditem.cost = solditem.cost_price*quantity; 
-	solditem.success = 1;
-	
-	return solditem;
-}
 
+
+typedef struct{
+	int saleid;
+	int itemid;
+	int userid;
+} MaxIDs;
 
 //define the item sale data structure
 typedef struct{
@@ -179,19 +49,7 @@ typedef struct{
 	float price;
 } SaleItems;
 
-SaleItems CreateNewSaleItems(Sale sale, SoldItem items[], int itemlen){
-	int i;
-	float total_price = 0;
-	SaleItems saleitem;
-	saleitem.saleid = sale.saleid;
-	memcpy(saleitem.items, items, sizeof(items));
-	for(i=0;i<itemlen;i++){
-		total_price = total_price+items[i].cost;
-	}
-	saleitem.price = total_price;
-	return saleitem;
-	
-}
+
 
 //define the user data structure
 
@@ -213,6 +71,7 @@ typedef struct{
 	char lname[20];
 	DOB dob;
 	char role[20];
+	int deleted ;
 } User;
 
 
@@ -248,6 +107,50 @@ char *PrintDOB(DOB dob){
 	return date;
 }
 
+SaleItems CreateNewSaleItems(Sale sale, SoldItem items[], int itemlen){
+	int i;
+	float total_price = 0;
+	SaleItems saleitem;
+	saleitem.saleid = sale.saleid;
+	memcpy(saleitem.items, items, sizeof(items));
+	for(i=0;i<itemlen;i++){
+		total_price = total_price+items[i].cost;
+	}
+	saleitem.price = total_price;
+	return saleitem;
+	
+}
+
+
+Sale CreateNewSale(int id, User user){
+	Sale sale;
+	sale.saleid = id;
+	sale.userid = user.userid;
+	sale.unix_time = time(NULL);
+	return sale;
+}
+
+
+SoldItem CreateNewSoldItem(Item item, int quantity){
+	
+	SoldItem solditem;
+	
+	
+	if (item.instock < 1 || item.instock == NULL){
+		solditem.success = 0;
+		return solditem;
+	}else if (quantity < 1){
+		solditem.success = 0;
+		return solditem;
+	}
+	
+	solditem.itemid = item.itemid;
+	solditem.quantity = quantity;
+	solditem.cost = item.cost_price*quantity; 
+	solditem.success = 1;
+	
+	return solditem;
+}
 
 
 AccountInfo CreateNewAccount(char uname[], char pword[]){
@@ -259,6 +162,21 @@ AccountInfo CreateNewAccount(char uname[], char pword[]){
 	
 	return account;
 }
+
+
+Item CreateNewItem(int id, char name[], int stock, float selling, float cost){
+	
+	Item item;
+	
+	item.itemid = id;
+	item.instock = stock;
+	item.selling_price = selling;
+	item.cost_price = cost; 
+	strcpy(item.name, name);
+	
+	return item;
+}
+
 
 
 DOB CreateNewDOB(char dayob[], char monthob[], char yearob[]){
@@ -273,12 +191,13 @@ DOB CreateNewDOB(char dayob[], char monthob[], char yearob[]){
 }
 
 
-User CreateNewUser(int id, AccountInfo account, char fname[], char lname[], DOB dob, char role[]){
+User CreateNewUser(int id, AccountInfo account, char fname[20], char lname[20], DOB dob, char role[20]){
 	
 	User user;
 	user.userid = id;
 	user.account = account;
 	user.dob = dob; 
+	user.deleted = 0;
 	strcpy(user.fname, fname);
 	strcpy(user.lname,lname);
 	strcpy(user.role,role);
@@ -287,17 +206,116 @@ User CreateNewUser(int id, AccountInfo account, char fname[], char lname[], DOB 
 }
 
 
+int IsNewFile(char filename[30]){
+	//open file and store it in the  file pointer
+	FILE *file = fopen(filename, "a+");
+	
+    if(file == NULL){
+    	//if file is empty return 0
+    	return 0;
+	}
+	//lookthrough the file to the end
+    fseek(file, 0, SEEK_END);
+    //store the size of the file in size
+    int size = ftell(file);
+    //close the file
+    fclose(file);
+	//return the size of the file
+    return size;
+}
 
-void PrintAllUsers(User users[], int start){
+
+
+int AddUserData(User users[], int length){
+	MaxIDs id;
+	int isemptyID = IsNewFile(IDFILE);
+	FILE *userdata, *iddata;
+	int i = 0;
+	int userid = 0;
+	if(isemptyID != 0){
+		iddata = fopen(IDFILE, "r");
+		while(fread(&id, sizeof( MaxIDs), 1, iddata)){
+			//loop through all structures in ineventory file
+			userid = id.userid;
+			
+		}
+		fclose(iddata);
+	
+	}else{
+		userid = -1;
+		userdata = fopen(USERFILE, "wb");
+		for(i=0;i<length;i++){
+			userid++;
+			users[i].userid = userid;
+			fwrite(&users[i], sizeof(User), 1, userdata);
+		}
+		fclose(userdata);
+		
+		iddata = fopen(IDFILE, "wb");
+		id.userid = userid;
+		fwrite(&id, sizeof( MaxIDs), 1, iddata);
+		fclose(iddata);
+		return;
+	}
+	
+	userdata = fopen(USERFILE, "ab");
+	for(i=userid;i<userid+length;i++){
+		userid++;
+		users[i].userid = userid;
+		fwrite(&users[i], sizeof(User), 1, userdata);
+	}
+	fclose(userdata);
+	iddata = fopen(IDFILE, "wb");
+	id.userid = userid;
+	fwrite(&id, sizeof( MaxIDs), 1, iddata);
+	fclose(iddata);
+	
+}
+
+
+int SeachForUser(){
+	return 0;
+}
+
+int SaveUsers(User user[]){
+	return 0;
+}
+
+
+
+void PrintAllUsers(int start, int end){
 	//printf("%d", length);
+	FILE *userdata;
+	User users[1000];
+	User user;
+	int count = 0;
+	if (IsNewFile(USERFILE) == 0){
+		return;
+	}
+	userdata = fopen(USERFILE, "r");
+	
+	while(fread(&user, sizeof( User), 1, userdata)){
+		//loop through all structures in ineventory file
+		users[count] = user;
+		count++;
+	}
+	
+	fclose(userdata);
+	if(count < end){
+		end = count;
+	}
 	int i = 0;
 	printf("+------------+-----------------------+-----------------------+-----------------------+-------------+\n");
 	printf("|     User ID|             First Name|              Last Name|               Position|          DOB|\n");
 	printf("+------------+-----------------------+-----------------------+-----------------------+-------------+\n");
-	for(i=start;i<start+20;i++){
-		printf("|%12s|%23s|%23s|%23s|%13s|\n", users[i].userid, users[i].fname, users[i].lname, users[i].role, PrintDOB(users[i].dob));
-		printf("+------------+-----------------------+-----------------------+-----------------------+-------------+\n");
+	for(i=start;i<end;i++){
+		if (users[i].deleted == 0){
+			printf("|%12d|%23s|%23s|%23s|%13s|\n", users[i].userid, users[i].fname, users[i].lname, users[i].role, PrintDOB(users[i].dob));
+			printf("+------------+-----------------------+-----------------------+-----------------------+-------------+\n");
+		}
+		
 	}
+	return;
 }
 
 
@@ -311,6 +329,7 @@ void PrintAllItems(Item items[], int start){
 		printf("|%8d|%46s|%11d|%16f|%13s|\n", items[i].itemid, items[i].name, items[i].instock, items[i].selling_price, items[i].cost_price);
 		printf("+------------+-----------------------+-----------------------+-----------------------+-------------+\n");
 	}
+	return;
 }
 
 
@@ -334,13 +353,26 @@ int main(int argc, char *argv[]) {
 	//char data[20];
 	DOB dob = CreateNewDOB("28", "05", "2004");
 	AccountInfo account = CreateNewAccount("quashman", "pop123");
-	User users[2] = {CreateNewUser(account, "Drew", "Quashie", dob, "manager"), CreateNewUser(account, "Drew", "Quashie", dob, "manager")};
+	User users[2] = {CreateNewUser(0, account, "Drew", "Quashie", dob, "manager"), CreateNewUser(0, account, "Drew", "Quashie", dob, "manager")};
 	//strcpy(data,  UserId(p1));
+	//AddUserData(users, 2);
 	int length = (sizeof(users) / sizeof(users[0]));
-	PrintAllUsers(users, length);
+	//printf("length %d", length);
+	PrintAllUsers(0,4);
+	
+	char idfile[20] = "IdTracker.bin";
+	//int size = isNewFile(idfile);
+	char userfile[20] = "UserDataBase.bin";
+	//AddUserData(users, 2, size, userfile,idfile);
+	
+	
 	return 0;
 }
 
+
+int login(char username[20], char password[20]){
+	
+}
 
 
 
